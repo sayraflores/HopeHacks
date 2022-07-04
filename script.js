a5bb3e59c07b98950fec85462bcab432e525aafc
Kyla Wootson <kylawootson@MacBook-Pro.local>
2022-07-04 19:52:51 -0400

    Adding to js file

--- script.js
+++ script.js
@@ -7,7 +7,25 @@ function myFunction() {
     }
 }
 
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('ckng4qPlLNlAPFv0D');
+})();
 
+window.onload = function() {
+    document.getElementById('contact_form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
 
  // SLIDESHOW STARTS HERE// 
 
